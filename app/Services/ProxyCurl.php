bdfd0876bee7f123139d80c3e370fd79471f5e73
stoneworld <stoneworld1991@gmail.com>
2018-02-27 23:35:50 +0800

    fix

--- app/Services/ProxyCurl.php
+++ app/Services/ProxyCurl.php
@@ -1,14 +1,17 @@
 <?php
+
 namespace App\Services;
+
 use DB;
 use Log;
 
 class ProxyCurl
 {
-	protected $headers = ['Accept: */*', 'Accept-Encoding: gzip,deflate,sdch', 'Accept-Language: zh-CN,zh;q=0.8,gl;q=0.6,zh-TW;q=0.4', 'Connection: keep-alive', 'Content-Type: application/x-www-form-urlencoded', 'Host: music.163.com', 'Referer: http://music.163.com/search/', 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.152 Safari/537.36'];
-	public function curl($url, $data = null, $referer='http://music.163.com/', $ip)
+    protected $headers = ['Accept: */*', 'Accept-Encoding: gzip,deflate,sdch', 'Accept-Language: zh-CN,zh;q=0.8,gl;q=0.6,zh-TW;q=0.4', 'Connection: keep-alive', 'Content-Type: application/x-www-form-urlencoded', 'Host: music.163.com', 'Referer: http://music.163.com/search/', 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.152 Safari/537.36'];
+
+    public function curl($url, $data = null, $referer = 'http://music.163.com/', $ip)
     {
-    	$curl = curl_init();
+        $curl = curl_init();
         curl_setopt($curl, CURLOPT_URL, $url);
         if ($data) {
             if (is_array($data)) $data = http_build_query($data);
@@ -21,47 +24,47 @@ class ProxyCurl
         curl_setopt($curl, CURLOPT_HTTPHEADER, $this->headers);
         curl_setopt($curl, CURLOPT_ENCODING, 'application/json');
         curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-        if(!empty($ip)) {
-        	curl_setopt($curl, CURLOPT_PROXY, $ip);
+        if (!empty($ip)) {
+            curl_setopt($curl, CURLOPT_PROXY, $ip);
         }
         $result = curl_exec($curl);
         if (curl_errno($curl)) {
-	        $http_status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-	        Log::info(curl_error($curl));
-	        echo "curl Request Error: $url " . curl_error($curl) . " $http_status \n";
-	        return '';
-    	}
-    	if (empty($result)) {
-    		return '';
-    	}
+            $http_status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+            Log::info(curl_error($curl));
+            echo "curl Request Error: $url " . curl_error($curl) . " $http_status \n";
+            return '';
+        }
+        if (empty($result)) {
+            return '';
+        }
         curl_close($curl);
         return $result;
     }
 
-    public function retryCurl($url, $data = null, $referer='http://music.163.com/', $retry=15)
+    public function retryCurl($url, $data = null, $referer = 'http://music.163.com/', $retry = 15)
     {
-    	$try = 0;
-    	$ip = '';
-    	$result = $this->curl($url, $data, $referer, $ip);
-    	while(empty($result)) {
-    		if ($try <= $retry) {
-    			$ip = $this->getProxyIp();
-	            $result = $this->curl($url, $data, $referer,$ip);
-	            $try++;
-	            echo "retry url: $try $url \n";
-	        } else {
-	            echo "fail retry url: $try $url \n";
-	            break;
-	        }
-    	}
-    	return $result;
+        $try = 0;
+        $ip = '';
+        $result = $this->curl($url, $data, $referer, $ip);
+        while (empty($result)) {
+            if ($try <= $retry) {
+                $ip = $this->getProxyIp();
+                $result = $this->curl($url, $data, $referer, $ip);
+                $try++;
+                echo "retry url: $try $url \n";
+            } else {
+                echo "fail retry url: $try $url \n";
+                break;
+            }
+        }
+        return $result;
     }
 
     public function getProxyIp()
     {
-    	$ips = DB::table('proxy_ips')->get()->toArray();
-    	$key = array_rand($ips, 1);
-    	$ip = $ips[$key]->ip.':'.$ips[$key]->port;
-    	return $ip;
+        $ips = DB::table('proxy_ips')->get()->toArray();
+        $key = array_rand($ips, 1);
+        $ip = $ips[$key]->ip . ':' . $ips[$key]->port;
+        return $ip;
     }
 } 
