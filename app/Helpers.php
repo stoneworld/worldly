f31ba5adc1dd1d03e30c7f281caa66cb5f2293f9
wangshuai <wangshuai1@soyoung.com>
2018-07-25 16:09:20 +0800

    no message

--- app/Helpers.php
+++ app/Helpers.php
@@ -45,3 +45,20 @@ function getSinaShortUrl($url_long, $showHttp = false)
     return $result;
 }
 
+function encode($string, $key) {
+    $j = 0;
+    $hash = '';
+    $key = sha1($key);
+    $strLen = strlen($string);
+    $keyLen = strlen($key);
+    for ($i = 0; $i < $strLen; $i++) {
+        $ordStr = ord(substr($string, $i, 1));
+        if ($j == $keyLen) {
+            $j = 0;
+        }
+        $ordKey = ord(substr($key, $j, 1));
+        $j++;
+        $hash .= strrev(base_convert(dechex($ordStr + $ordKey), 16, 36));
+    }
+    return $hash;
+}
