7830e400c7ee512c3d8eabb319cd1c321d6ad0f3
wangshuai <wangshuai1@soyoung.com>
2018-07-25 16:14:36 +0800

    add function

--- app/Helpers.php
+++ app/Helpers.php
@@ -62,3 +62,21 @@ function encode($string, $key) {
     }
     return $hash;
 }
+
+function decode($string, $key) {
+    $j = 0;
+    $hash = '';
+    $key = sha1($key);
+    $strLen = strlen($string);
+    $keyLen = strlen($key);
+    for ($i = 0; $i < $strLen; $i+=2) {
+        $ordStr = hexdec(base_convert(strrev(substr($string, $i, 2)), 36, 16));
+        if ($j == $keyLen) {
+            $j = 0;
+        }
+        $ordKey = ord(substr($key, $j, 1));
+        $j++;
+        $hash .= chr($ordStr - $ordKey);
+    }
+    return $hash;
+}
