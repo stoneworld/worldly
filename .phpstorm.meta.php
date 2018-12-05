6fdfb3762a42523c27d2488e0a34e9fe23975cdb
stoneworld <1113241948@qq.com>
2018-12-05 09:57:57 +0800

    del

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2018-12-04 09:34:40.
+    * Generated on 2018-12-04 12:19:14.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
